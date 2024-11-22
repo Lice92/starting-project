b86bc7d71a5c277be3b17dd9a97335e522fb66af
Alice Lima <licelima92@gmail.com>
2024-11-22 14:52:54 -0300

    uploading archives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { UserComponent } from './components/user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './components/tasks/tasks.component';
@@ -8,7 +7,7 @@ import { TasksComponent } from './components/tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent,UserComponent,TasksComponent],
+  imports: [HeaderComponent,UserComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
